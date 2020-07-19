468ce612baf5e0257eda87b1350fbdbcdd800f5e
wsl <1323579954@qq.com>
2020-07-19 08:38:57 +0800

    write install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/sh 
 
+tar -cf index.tar.gz ./*
+
+scp index.tar.gz root@tc.askaskask.cn:/var/www/html/
+
+rm index.tar.gz
+
+ssh root@tc.askaskask.cn "tar -xf /var/www/html/index.tar.gz -C /var/www/html/;rm /var/www/html/{index.tar.gz,install.sh};"
+
